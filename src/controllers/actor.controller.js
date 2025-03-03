b86f978d14573b09a6cb39883491949332c85704
GuarU345 <abelmtz34@outlook.com>
2025-03-02 18:48:57 -0600

    complete actor CRUD

--- src/controllers/actor.controller.js
+++ src/controllers/actor.controller.js
@@ -9,6 +9,55 @@ const getActors = async(_req,res) => {
     }
 }
 
+const getActorById = async(req,res) => {
+    const {id} = req.params
+
+    try {
+        const actor = await actorService.getActorById(Number(id))
+        res.json(actor)
+    } catch (error) {
+        res.json(error)
+    }
+}
+
+const createActor = async(req,res) => {
+    const data = req.body
+
+    try {
+        const newActor = await actorService.createActor(data)
+        res.json(newActor) 
+    } catch (error) {
+        res.json(error)
+    }
+}
+
+const updateActor = async(req,res) => {
+    const {id} = req.params
+    const data = req.body
+
+    try {
+        const updatedActor = await actorService.updateActor(Number(id),data)
+        res.json(updatedActor)
+    } catch (error) {
+        res.json(error)
+    }
+}
+
+const deleteActor = async(req,res) => {
+    const {id} = req.params
+
+    try {
+        const deleted = await actorService.deleteActor(Number(id))
+        res.json(deleted)
+    } catch (error) {
+        res.json(error)
+    }
+}
+
 export const actorController = {
-    getActors
+    getActors,
+    getActorById,
+    createActor,
+    updateActor,
+    deleteActor
 }
--- src/routes/actor.routes.js
+++ src/routes/actor.routes.js
@@ -4,5 +4,9 @@ import { actorController } from "../controllers/actor.controller.js";
 const router = Router()
 
 router.get('/actors',actorController.getActors)
+router.get('/actors/:id',actorController.getActorById)
+router.post('/actors',actorController.createActor)
+router.put('/actors/:id',actorController.updateActor)
+router.delete('/actors/:id',actorController.deleteActor)
 
 export const actorRoutes = router
