a902233fd9ed7a2c2c579e259c6f283d4d33468e
z-Nava <62721391+z-Nava@users.noreply.github.com>
2025-03-10 17:35:01 -0600

    config route film category

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { actorRoutes } from "./actor.routes.js";
+import { filmCategoryRoutes } from "./film_category.routes.js";
 
 export const router = new Router()
 
 router.use(actorRoutes)
+router.use(filmCategoryRoutes)
