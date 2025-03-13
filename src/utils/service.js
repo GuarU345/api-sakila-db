1895c4a499573699d2381046b7d6abc18341a779
abel.dominguez <abel.dominguez@transmontes.com>
2025-03-13 13:23:04 -0600

    change enums for rating in prisma model and try and catch service errors

--- src/utils/service.js
+++ src/utils/service.js
@@ -1,26 +1,46 @@
 export class CrudService {
-    constructor(model,primaryKey) {
+    constructor(model, primaryKey) {
         this.model = model
         this.primaryKey = primaryKey
     }
 
     async create(data) {
-        return await this.model.create({ data })
+        try {
+            return await this.model.create({ data })
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
     async read() {
-        return await this.model.findMany()
+        try {
+            return await this.model.findMany()
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
     async readOne(id) {
-        return await this.model.findUnique({ where: { [this.primaryKey]: id } })
+        try {
+            return await this.model.findUnique({ where: { [this.primaryKey]: id } })
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
     async update(id, data) {
-        return await this.model.update({ where: { [this.primaryKey]: id }, data })
+        try {
+            return await this.model.update({ where: { [this.primaryKey]: id }, data })
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
     async delete(id) {
-        return await this.model.delete({ where: { [this.primaryKey]: id } })
+        try {
+            return await this.model.delete({ where: { [this.primaryKey]: id } })
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 }
