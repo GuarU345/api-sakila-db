089121383ab5f9020efe5c0337800c63b2883d73
GuarU345 <abelmtz34@outlook.com>
2025-03-18 18:17:03 -0600

    add route to show a specific film text

--- src/routes/film_text.routes.js
+++ src/routes/film_text.routes.js
@@ -4,6 +4,7 @@ import { FilmTextController } from "../controllers/film_text.controller.js";
 const router = Router()
 const filmTextController = new FilmTextController()
 
+router.get('/film_texts/:id',filmTextController.readOne.bind(filmTextController))
 router.post('/film_texts', filmTextController.create.bind(filmTextController))
 
 export const filmTextRoutes = router
